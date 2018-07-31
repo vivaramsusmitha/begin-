0447027688f697822a0652733c61a805c3948523
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-07-31 12:40:36 +0530

    Update primenums.java

--- primenums.java
+++ primenums.java
@@ -9,51 +9,17 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
-		/* package whatever; // don't place package name! */
-
-
-		// your code goes here
-		int n,k;
-     Scanner s=new Scanner(System.in);
-     n=s.nextInt();
-     k=s.nextInt();
-
-        int m=0;
-	  
-            for(int i = n+1; i <k; i++) 
-	    {
-	    	int l=0;
-                // condition for nonprime number
-		    for(int j=1;j<=i;j++)
-		    {
-                                if(i%j == 0)
-		         {
-                                    l++;
-		         }
-		    }
-	    
-		    if(l==2)
-		    {
-			if(m<1){
-				
-			
-			   
-			      System.out.print(i);
-			    }
-			    else
-			    {
-				    System.out.print(" ");
-				    System.out.print(i);
-			    }m++;
-		    }
-            }
-
-           
-        
+		
+		int m,n;
+		Scanner s=new Scanner(System.in);
+		m=s.nextInt();
+		n=s.nextInt();
+	    for(int i=m+1; i<n; i++)
+        {
+            if(i%2==0)
+                System.out.print(i+" ");
+        }  
+			System.out.println();
 	
 	}
-	}
-	
-
-	
+}
