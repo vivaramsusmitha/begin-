0b5e50a1088d4ed07bfbe1e463940d091c82a87d
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-01 14:43:19 +0530

    Create arrmin.java

---
+++ arrmin.java
@@ -0,0 +1,41 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int small, size, i;
+       int arr[] = new int[50];
+       Scanner scan = new Scanner(System.in);
+	   
+      // System.out.print("Enter Array Size : ");
+       size = scan.nextInt();
+	   
+       System.out.print("Enter Array Elements : ");
+       for(i=0; i<size; i++)
+       {
+           arr[i] = scan.nextInt();
+       }
+	   
+       //System.out.print("Searching for the Smallest Element....\n\n");
+	   
+       small = arr[0];
+	   
+       for(i=0; i<size; i++)
+       {
+           if(small > arr[i])
+           {
+               small = arr[i];
+           }
+           
+       }
+	   
+       System.out.print( + small); 
+   }
+	}
