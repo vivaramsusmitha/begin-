7911dddf3a5a47894b148002394dfd705b093aa8
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-02 14:38:14 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,44 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n, temp;
+        Scanner s = new Scanner(System.in);
+        //System.out.print("Enter no. of elements you want in array:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        //System.out.println("Enter all the elements:");
+        for (int i = 0; i < n; i++) 
+        {
+            a[i] = s.nextInt();
+        }
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
+           {
+                if (a[i] > a[j]) 
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        //System.out.print("ascending order");
+        for (int i = 0; i < n - 1; i++) 
+        {
+            System.out.print(a[i] + "\t");
+        }
+        System.out.print(a[n - 1]);
+    
+         
+	}
+}
