7559e971557327248bed44626fcd571c80a1c483
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-02 15:30:48 +0530

    Create time.java

---
+++ time.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class time
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in = new Scanner(System.in);
+
+		// enter the seconds here.
+		//System.out.print("Enter seconds : ");
+
+		int seconds = in.nextInt(); 
+
+		int p1 = seconds % 60;
+		int p2 = seconds / 60;
+		//int p3 = p2 % 60;
+
+		p2 = p2 / 60;
+
+		System.out.print( p2 + " " + p1);
+		System.out.print("\n");
+	}  
+}
+
+
+
+	
